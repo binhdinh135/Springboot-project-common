0c392d34856ba7c7514ba507700c1b0bbf52c7c1
binhdinh135 <binhdinh1351@gmai.com>
2024-08-26 14:27:56 +0700

    Check unique field.

--- src/main/java/com/binhdc/springbootcommonproject/entity/User.java
+++ src/main/java/com/binhdc/springbootcommonproject/entity/User.java
@@ -20,7 +20,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Column(name = "username", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    @Column(name = "username"
+            /*
+            unique = true để xác định rằng với cột username chỉ có duy nhất 1 giá trị tốn tại.
+            Không truùng giá trị. Với các bài toàn nhiều người dùng cùng một lúc gọi API tạo user/pass
+            -> "unique = true" sẽ check và chỉ duy nhất 1 request tạo ra tài khoản.
+            Chúng ta có thể test bằng Apache Jmeter
+             */
+            , unique = true
+//            , columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci"
+    )
     String username;
 
     String password;
